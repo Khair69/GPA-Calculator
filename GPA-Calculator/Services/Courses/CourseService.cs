ce471a80d3a0c2eeb0a3d3c9f1f2a043c09f9171
Khair <khair04@outlook.com>
2025-05-19 13:55:50 +0300

    added course service layer

---
+++ GPA-Calculator/Services/Courses/CourseService.cs
@@ -0,0 +1,50 @@
+﻿using GPA_Calculator.Data;
+using GPA_Calculator.Modles;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+
+namespace GPA_Calculator.Services.Courses
+{
+    public class CourseService : ICourseService
+    {
+        private readonly AppDbContext _context;
+
+        public CourseService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AddCourseAsync(Course course)
+        {
+            _context.Courses.Add(course);
+
+            var res = await _context.SaveChangesAsync();
+            return res == 1;
+        }
+
+        public async Task<IList<Course>> GetAllCoursesAsync()
+        {
+            return await _context.Courses
+                .OrderBy(c => c.Year)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<Course> GetCourseByIdAsync(string id)
+        {
+            return await _context.Courses
+                .AsNoTracking()
+                .FirstOrDefaultAsync(c => c.CourseId == id);
+        }
+
+        public Task<bool> DeleteCourseAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateCourseAsync(Course course)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GPA-Calculator/Services/Courses/ICourseService.cs
@@ -0,0 +1,13 @@
+﻿using GPA_Calculator.Modles;
+
+namespace GPA_Calculator.Services.Courses
+{
+    public interface ICourseService
+    {
+        Task<IList<Course>> GetAllCoursesAsync();
+        Task<Course> GetCourseByIdAsync(string id);
+        Task<bool> AddCourseAsync(Course course);
+        Task<bool> DeleteCourseAsync(string id);
+        Task<bool> UpdateCourseAsync(Course course);
+    }
+}

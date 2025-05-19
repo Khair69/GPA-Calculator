85f28fd011ff7c5a4e3ee4abd89d51cdbf67aa9d
Khair <khair04@outlook.com>
2025-05-19 13:19:19 +0300

    added the course model

---
+++ GPA-Calculator/Modles/Course.cs
@@ -0,0 +1,17 @@
+﻿namespace GPA_Calculator.Modles
+{
+    public class Course
+    {
+        public string CourseId { get; set; }
+
+        public string CourseName { get; set; }
+
+        public int Hours { get; set; }
+
+        public int Grade { get; set; }
+
+        public int Year { get; set; }
+
+        public int Semester {  get; set; }
+    }
+}

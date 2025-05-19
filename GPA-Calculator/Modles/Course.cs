fd39644f65017aba6ebd924e3f01d966d925d4ab
Khair <khair04@outlook.com>
2025-05-19 19:23:10 +0300

    added data anotation to the model

--- GPA-Calculator/Modles/Course.cs
+++ GPA-Calculator/Modles/Course.cs
@@ -6,25 +6,32 @@ namespace GPA_Calculator.Modles
     {
         [Required]
         [Display(Name ="ID")]
-        public string CourseId { get; set; }
+        [StringLength(10, MinimumLength = 2, ErrorMessage = "the {0} must be at least {2} and at most {1}")]
+        public required string CourseId { get; set; }
 
         [Required]
         [Display(Name = "Name")]
-        public string CourseName { get; set; }
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "the {0} must be at least {2} and at most {1}")]
+        public required string CourseName { get; set; }
 
         [Required]
+        [Range(1,4, ErrorMessage ="{0} must be between {1} and {2}")]
         public int Hours { get; set; }
 
         [Required]
+        [Range(0, 100, ErrorMessage = "{0} must be between {1} and {2}")]
         public int Grade { get; set; }
 
         [Required]
+        [Range(1, 5, ErrorMessage = "{0} must be between {1} and {2}")]
         public int Year { get; set; }
 
         [Required]
+        [Range(1, 3, ErrorMessage = "{0} must be between {1} and {2}")]
         public int Semester {  get; set; }
 
         [Required]
+        [Range(0, 4, ErrorMessage = "{0} must be between {1} and {2}")]
         public float Points { get; set; }
     }
 }
